20b0de3d0550cb3820ab59539b51b129728b6e9b
eduardo bandeira <fustecdev@gmail.com>
2025-10-07 00:32:27 -0300

    criando map

---
+++ meses.js
@@ -0,0 +1,19 @@
+
+var setembrovalor=[
+    "R$ 100,00",
+     "R$ 70,00",
+      "R$ 30,00",
+       "R$ 60,00",
+        "R$ 80,00",
+         "R$ 100,00",
+          "R$ 50,00",
+           "Total R$ 490,00 do dia 7/9/20250 a 7/10/2025",
+
+]
+
+var mes = [
+    0,
+    setembrovalor,
+]
+
+export {mes};
--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-//alert("script Fucionando")
+import { mes } from "./meses.js";
+import { mesimg } from "./img.js";
 
 //pegando o select** Selecione o elemento <select>
 const select = document.querySelector(".mes");
 
 
-
-
-
-
-
 const busca = document.querySelector(".buscar")
 const p1 = document.querySelector(".p1")
+const iputImag = document.querySelector(".iputImag")
+const img = document.querySelector(".img")
+const pai = document.querySelector(".pai")
 
-
+var posiput = 0
+var cont2 = 0
 busca.addEventListener("click",()=>{
+ 
  //pegar o opition**Acesse a option selecionada:
 const selectOption = select.options[select.selectedIndex];
 const opitionText = select.options[select.selectedIndex].text;  
@@ -21,9 +22,37 @@ const opitionText = select.options[select.selectedIndex].text;
 // Obtenha o value da option
 
 const valorDaOption = selectOption.value;
+iputImag.style.display="block"
+posiput[valorDaOption]
+
 p1.textContent="Mês:  "+opitionText
- console.log(valorDaOption)   
+
+   var busqmes = mes[posiput]
+    busqmes.map(()=>{
+
+    
+ var p2 = document.createElement("p")
+ p2.textContent=busqmes[cont2]
+
+ pai.appendChild(p2)
+ cont2++
+ })
+    
+
+
+});
+
+var cont = 0;
+
+iputImag.addEventListener("click", ()=>{
+    
+    var posImg = mesimg[posiput]
+    img.src= posImg[cont]
+    
+    cont++ 
+    if (cont==posImg.length) {
+      cont=0  
+    }
 
 })
-
 
