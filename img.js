7989e822b9b259aff06919fd9a67b11ae045526f
eduardo bandeira <fustecdev@gmail.com>
2025-10-12 18:02:25 -0300

    salvar tudo

---
+++ img.js
@@ -0,0 +1,24 @@
+var setembroimg= [
+    "image/7-9-2025.jpg",
+     "image/14-9-2025.jpg",
+      "image/15-9-2025.jpg",
+       "image/21-9-2025.jpg",
+        "image/28-9-2025.jpg",
+         "image/3-10-2025.jpg",
+          "image/5-10-2025.jpg",
+
+]
+var outubro =[
+    "image/8-10-2025.jpg",
+    "image/12-10-2025.jpg",
+    
+
+]
+
+var mesimg =[
+    0,
+    setembroimg,
+    outubro,
+]
+
+export {mesimg};
---
+++ meses.js
@@ -0,0 +1,31 @@
+
+var setembrovalor=[
+    "R$ 100,00",
+     "R$ 70,00",
+      "R$ 30,00",
+       "R$ 60,00",
+        "R$ 80,00",
+         "R$ 100,00",
+          "R$ 50,00",
+           "Total R$ 490,00 do dia 7/9/20250 a 7/10/2025",
+
+]
+var outubro =[
+"R$ 100,00",
+"R$ 140,00",
+"",
+"",
+"",
+"",
+"",
+"Toata R$ 240,00"
+
+]
+
+var mes = [
+    0,
+    setembrovalor,
+    outubro,
+]
+
+export {mes};
