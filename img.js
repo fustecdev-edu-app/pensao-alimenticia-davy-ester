2b121035190435be7728eb0b16583241a588fb5d
eduardo bandeira <fustecdev@gmail.com>
2025-10-12 18:04:22 -0300

    tirou as img da pasta

--- img.js
+++ img.js
@@ -1,16 +1,16 @@
 var setembroimg= [
-    "image/7-9-2025.jpg",
-     "image/14-9-2025.jpg",
-      "image/15-9-2025.jpg",
-       "image/21-9-2025.jpg",
-        "image/28-9-2025.jpg",
-         "image/3-10-2025.jpg",
-          "image/5-10-2025.jpg",
+    "7-9-2025.jpg",
+     "14-9-2025.jpg",
+      "15-9-2025.jpg",
+       "21-9-2025.jpg",
+        "28-9-2025.jpg",
+         "3-10-2025.jpg",
+          "5-10-2025.jpg",
 
 ]
 var outubro =[
-    "image/8-10-2025.jpg",
-    "image/12-10-2025.jpg",
+    "8-10-2025.jpg",
+    "12-10-2025.jpg",
     
 
 ]
